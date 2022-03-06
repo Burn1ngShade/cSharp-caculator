df0e33a0bed0213bece7e71f6c5fda436ee2f72f
Burn1ngShade <92182982+Burn1ngShade@users.noreply.github.com>
2022-03-06 00:57:56 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
-﻿class Program
+class Program
 {
     static void Main(string[] args)
     {
+        //create a interface for the caculaotr
+        //thats all you have to type for it to work
         Caculator.CaculatorInterface();
     }
 }
